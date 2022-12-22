cae4394f205b9d522a0f71410a7ec967556b71b7
Noor Mohammed <noormohammedknr2@gmail.com>
2022-12-23 00:36:33 +0530

    Upto home page protofolio done

---
+++ assests/scripts/script.js
@@ -0,0 +1,16 @@
+const buttonEl = document.querySelector(".aboutme-btn");
+const aboutmeEl = document.querySelector(".about-me");
+const skillsEl = document.querySelector(".skills");
+const experienceEl = document.querySelector(".experience");
+
+const aboutMeChange = () => {
+  aboutmeEl.classList.toggle("toggle");
+};
+
+const skillsChange = () => {
+  skillsEl.classList.toggle("toggle");
+};
+
+const ExperienceChange = () => {
+  experienceEl.classList.toggle("toggle");
+};
