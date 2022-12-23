e0b789cbe3c90d3b3494efc92a1b80403b3d61c0
Noor Mohammed <noormohammedknr2@gmail.com>
2022-12-23 08:17:48 +0530

    Added schooling section

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -2,6 +2,7 @@ const buttonEl = document.querySelector(".aboutme-btn");
 const aboutmeEl = document.querySelector(".about-me");
 const skillsEl = document.querySelector(".skills");
 const experienceEl = document.querySelector(".experience");
+const schoolEl = document.querySelector(".schooling");
 
 const aboutMeChange = () => {
   aboutmeEl.classList.toggle("toggle");
@@ -13,4 +14,8 @@ const skillsChange = () => {
 
 const ExperienceChange = () => {
   experienceEl.classList.toggle("toggle");
+};
+
+const schoolChange = () => {
+  schoolEl.classList.toggle("toggle");
 };
